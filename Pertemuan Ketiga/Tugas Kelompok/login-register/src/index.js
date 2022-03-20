a36cbb46bbd148e9496acc44435a281424d0fb5b
Rasyid-deve <renaldirasyed@gmail.com>
2022-03-20 21:08:47 +0700

    Update Commit

--- Pertemuan Ketiga/Tugas Kelompok/login-register/src/index.js
+++ Pertemuan Ketiga/Tugas Kelompok/login-register/src/index.js
@@ -7,20 +7,20 @@ import FrmLogin from './component/FrmLogin';
 import FormRegister from './component/FormRegister';
 import reportWebVitals from './reportWebVitals';
 
-export default function FrmLogin() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element = {<FrmLogin />} />
-                <Route path="register" element={<FormRegister />} />
-            {/* </Route> */}
-            </Routes>
-        </BrowserRouter>
-    );
-}
+// export default function FrmLogin() {
+//     return (
+//         <BrowserRouter>
+//             <Routes>
+//                 <Route path="/" element = {<FrmLogin />} />
+//                 <Route path="register" element={<FormRegister />} />
+//             {/* </Route> */}
+//             </Routes>
+//         </BrowserRouter>
+//     );
+// }
 
-ReactDOM.render(<FrmLogin />, document.getElementById('root'));
-// ReactDOM.render(<FormRegister />, document.getElementById('root'));
+// ReactDOM.render(<FrmLogin />, document.getElementById('root'));
+ReactDOM.render(<FormRegister />, document.getElementById('root'));
 // ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
