954babbcc312875f31685917303c25291ec5dc31
Rasyid-deve <renaldirasyed@gmail.com>
2022-04-24 08:59:53 +0700

    week ke-10

---
+++ Firebase_Modul10/firebase-react/src/firebase.config.js
@@ -0,0 +1,31 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyC7NLE7-RC6CnGiXPmwf4tY2GJx7-VwX6A',
+//   authDomain: 'fir-react-68225.firebaseapp.com',
+//   projectId: 'fir-react-68225',
+//   storageBucket: 'fir-react-68225.appspot.com',
+//   messagingSenderId: '422110860286',
+//   appId: '1:422110860286:web:3f1d5c10759b218fb1a6af',
+//   measurementId: 'G-PEHPDLE9EB',
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export default {
+  apiKey: 'AIzaSyC7NLE7-RC6CnGiXPmwf4tY2GJx7-VwX6A',
+  authDomain: 'fir-react-68225.firebaseapp.com',
+  projectId: 'fir-react-68225',
+  storageBucket: 'fir-react-68225.appspot.com',
+  messagingSenderId: '422110860286',
+  appId: '1:422110860286:web:3f1d5c10759b218fb1a6af',
+  measurementId: 'G-PEHPDLE9EB',
+};
