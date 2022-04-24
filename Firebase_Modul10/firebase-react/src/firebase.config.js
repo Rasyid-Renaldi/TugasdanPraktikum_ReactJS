f8a75d45e3a1e1aab259178b181296a3e53983c5
Rasyid-deve <renaldirasyed@gmail.com>
2022-04-25 04:55:26 +0700

    week ke-10

--- Firebase_Modul10/firebase-react/src/firebase.config.js
+++ Firebase_Modul10/firebase-react/src/firebase.config.js
@@ -1,18 +1,6 @@
 // import firebase from 'firebase/app';
 import 'firebase/auth';
 
-// const app = firebase.initializeApp({
-//   apiKey: 'AIzaSyC7NLE7-RC6CnGiXPmwf4tY2GJx7-VwX6A',
-//   authDomain: 'fir-react-68225.firebaseapp.com',
-//   projectId: 'fir-react-68225',
-//   storageBucket: 'fir-react-68225.appspot.com',
-//   messagingSenderId: '422110860286',
-//   appId: '1:422110860286:web:3f1d5c10759b218fb1a6af',
-//   measurementId: 'G-PEHPDLE9EB',
-// });
-// export default app;
-// export const auth = firebase.auth();
-
 export default {
   apiKey: 'AIzaSyC7NLE7-RC6CnGiXPmwf4tY2GJx7-VwX6A',
   authDomain: 'fir-react-68225.firebaseapp.com',
